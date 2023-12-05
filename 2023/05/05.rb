8e26244c03dd5608a45e86044a161c836d2803ac
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-12-05 15:23:56 +0100

    Day 5 - Part 2 brute force

--- 2023/05/05.rb
+++ 2023/05/05.rb
@@ -1,12 +1,14 @@
 # data = File.open('example').read
 data = File.open('input').read
 seeds = data.split("\n\n")[0].split(': ')[1].split.map(&:to_i)
+
+# Part 1
 charts = data.split("\n\n")[1..]
              .map { |chart| chart.split("\n")[1..].map { |l| l.split.map(&:to_i) } }
 
 locations = []
 
-def convert(seed, chart)
+def next_category(seed, chart)
   chart.each do |line|
     d_start, s_start, s_len = line
     return d_start + seed - s_start if seed.between? s_start, s_start + s_len - 1
@@ -15,11 +17,47 @@ def convert(seed, chart)
   seed
 end
 
-seeds.each do |seed|
+def convert(seed, charts)
   charts.each do |chart|
-    seed = convert(seed, chart)
+    seed = next_category(seed, chart)
   end
+
+  seed
+end
+
+seeds.each do |seed|
+  seed = convert(seed, charts)
   locations << seed
 end
 
 puts locations.min
+
+# Part 2
+seed_ranges = seeds.each_slice(2).to_a
+
+def reverse(value, reversed)
+  reversed.each do |chart|
+    chart.each do |line|
+      if value.between?(line[0], line[0] + line[2] - 1)
+        value = value - line[0] + line[1]
+        break
+      end
+    end
+  end
+
+  value
+end
+
+location = 0
+
+loop do
+  value = reverse(location, charts.reverse)
+  seed_ranges.each do |range|
+    if value.between? range.first, range.first + range.last - 1
+      puts location
+      exit
+    end
+  end
+
+  location += 1
+end
