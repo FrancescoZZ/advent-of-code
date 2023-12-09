5a51ebc722e172246bc5cea28ea6511897b849fa
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-12-09 10:14:34 +0100

    Day 9 cleanup

--- 2023/09/09.rb
+++ 2023/09/09.rb
@@ -1,31 +1,23 @@
 # frozen_string_literal: true
 
-# data = File.open('example')
 data = File.open('input')
            .read
            .split("\n")
            .map { |l| l.split.map(&:to_i) }
 
-# p data
-
 def next_value(hst)
-  differences = []
-  hst.each_cons(2) do |(m, n)|
-    differences << n - m
-  end
-  p differences
+  differences = hst.each_cons(2)
+                   .map { |(m, n)| n - m }
   hst.last + (differences.uniq.length == 1 ? differences.first : next_value(differences))
 end
 
-next_values = []
-prev_values = []
+next_values = data.map { next_value(_1) }
+prev_values = data.map { next_value(_1.reverse) }
 
-data.each do |hst|
-  p hst
-  next_values << next_value(hst)
-  prev_values << next_value(hst.reverse)
-  p next_values.last
-end
+# data.each do |hst|
+#   next_values << next_value(hst)
+#   prev_values << next_value(hst.reverse)
+# end
 
 p next_values.sum
 p prev_values.sum
