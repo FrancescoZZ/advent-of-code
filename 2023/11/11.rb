1fc69985a435c55ad07256c755db83bd2a216bf2
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-12-13 16:45:25 +0100

    refactored for part 1 and part 2

--- 2023/11/11.rb
+++ 2023/11/11.rb
@@ -1,40 +1,36 @@
 # frozen_string_literal: true
 
-require 'set'
-
-# data = File.open('input').read.split("\n")
 data = File.open('input').read.split("\n").map(&:chars)
 
-# N = 10
 def distance((ar, ac), (br, bc))
   (ar - br).abs + (ac - bc).abs
 end
 
-def sum_of_distances(map, exp)
-  expanded = map.flat_map { |row| row.all?('.') ? [row] * exp : [row] }
-                .transpose
-                .flat_map { |col| col.all?('.') ? [col] * exp : [col] }
-                .transpose
-  cols = expanded.first.length
-
-  flat = expanded.flatten
-  galaxies = flat.each_index
-                 .select { |i| flat[i] == '#' }
-                 .map { |i| i.divmod(cols) }
-
-  steps = 0
-  galaxies.each_with_index do |galaxy, i|
-    galaxies[i + 1..].each do |other|
-      steps += distance(galaxy, other)
-    end
-  end
-
-  steps
+def expand(data, factor)
+  data.flat_map { |row| row.all?('.') ? [row] * factor : [row] }
+      .transpose
+      .flat_map { |col| col.all?('.') ? [col] * factor : [col] }
+      .transpose
 end
 
-part_one = sum_of_distances(data, 2)
+def galaxies(map)
+  cols = map.first.length
+
+  flat = map.flatten
+  flat.each_index
+      .select { |i| flat[i] == '#' }
+      .map { |i| i.divmod(cols) }
+end
+
+def sum_of_distances(galaxies)
+  galaxies.each_with_index.reduce(0) do |steps, (galaxy, i)|
+    steps + galaxies[i + 1..].reduce(0) { |g_steps, other| g_steps + distance(galaxy, other) }
+  end
+end
+
+part_one = sum_of_distances(galaxies(expand(data, 2)))
 p part_one
 
-base = sum_of_distances(data, 1)
+base = sum_of_distances(galaxies(expand(data, 1)))
 increase = part_one - base
 p base + (increase * (1_000_000 - 1))
