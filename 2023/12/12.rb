b4f33bd01510773e20b3bed5106876bd97bfb085
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-12-13 16:42:18 +0100

    Day 12

--- 2023/12/12.rb
+++ 2023/12/12.rb
@@ -1,39 +1,53 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics
-
 input = File.open('input').read
 
 schematic = input.split("\n")
-                 .map { |l| l.split.each_with_index.map { |p, i| i == 1 ? p.split(',').map(&:to_i) : p } }
+                 .map { |l| l.split.map.with_index { |p, i| i == 1 ? p.split(',').map(&:to_i) : p } }
 
-count = 0
-
-schematic.each do |str, groups|
-  ['.', '#'].repeated_permutation(str.count('?')).to_a.each do |comb|
-    new_str = str.dup
-    comb.length.times { new_str.sub!('?', comb.shift) }
-    count += 1 if new_str.split('.').reject(&:empty?).map(&:length) == groups
-  end
+def valid_segment?(str, groups)
+  str[0] =~ /[#?]/ &&                                  # Character is or could be '#'
+    str.length >= groups.first &&                      # Enough space for required number of '#'
+    str[...groups.first] !~ /\./ &&                    # No '.' within the group's length. A '.' implies multiple groups
+    (str.length == groups[0] || str[groups[0]] != '#') # If str length ≠ group length, ensure next character isn't '#'
 end
 
-p count
+def skip_dots_and_question_marks(str, groups, memo)
+  return 0 unless str[0] =~ /[.?]/
 
-# p schematic
+  next_str = str[1..]
+  memo[[next_str, groups]] = arrangements(next_str, groups, memo)
+end
 
-# unfolded = schematic.map do |(str, groups)|
-#   [str.concat('?'.concat(str) * 4), groups * 5]
-# end
+def count_arrangements(str, groups, memo)
+  count = 0
 
-# count = 0
+  count += skip_dots_and_question_marks(str, groups, memo)
 
-# unfolded.each do |str, groups|
-#   p str
-#   ['.', '#'].repeated_permutation(str.count('?')).to_a.each do |comb|
-#     new_str = str.dup
-#     comb.length.times { new_str.sub!('?', comb.shift) }
-#     count += 1 if new_str.split('.').reject(&:empty?).map(&:length) == groups
-#   end
-# end
+  return count unless valid_segment?(str, groups)
 
-# p count
+  next_segment = str[groups.first + 1..]
+  remaining_groups = groups[1..]
+
+  # Check the next segment recursively
+  memo[[next_segment, remaining_groups]] = arrangements(next_segment, remaining_groups, memo)
+  count + memo[[next_segment, remaining_groups]]
+end
+
+def arrangements(str, groups, memo = {})
+  # Base cases
+  return (groups.empty? ? 1 : 0) if str.nil? || str.empty? # String consumed, groups consumed / present
+  return (str.include?('#') ? 0 : 1) if groups.empty?      # Groups consumed, '#' still left in str
+  return memo[[str, groups]] if memo.key?([str, groups])   # Use values in memo to avoid having to compute again
+
+  count = count_arrangements(str, groups, memo)
+  memo[[str, groups]] = count
+end
+
+def solve(schematic, folds)
+  unfolded = schematic.map { |(str, groups)| [([str] * folds).join('?'), groups * folds] }
+  unfolded.reduce(0) { |total, (str, groups)| total + arrangements(str, groups) }
+end
+
+p solve(schematic, 1)
+p solve(schematic, 5)
