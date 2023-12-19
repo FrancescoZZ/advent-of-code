b934a58baf508b548279c5dcf9b45df1940ef497
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-12-19 01:21:55 +0100

    Day 18 cleanup

--- 2023/18/18.rb
+++ 2023/18/18.rb
@@ -1,9 +1,11 @@
-# rubocop:disable Metrics
-#
+# frozen_string_literal: true
+
 require 'set'
 
 input = File.open('input').read
-instructions = input.split("\n").map(&:split)
+instructions = input.split("\n")
+                    .map(&:split)
+                    .map { |(dir, length, hex)| [dir, length.to_i, hex] }
 
 def move(x, y, dir, length)
   case dir
@@ -21,40 +23,30 @@ def direction_groups(instructions)
     .chars.each_cons(3).map(&:join)
 end
 
-def find_area(instructions, directions)
-  x = 0
-  y = 0
-  trench = []
-
-  instructions.zip(directions).each do |((dir, length_s, _hex), group)|
-    length = length_s.to_i
+def dig_trench(instructions, directions, x = 0, y = 0)
+  instructions.zip(directions)
+              .each_with_object([]) do |((dir, length, _hex), group), arr|
     case group
-    when /URD|DLU/
-      length += 1
-    when /ULD|DRU/
-      length -= 1
-    when /RDL|LUR/
-      length += 1
-    when /LDR|RUL/
-      length -= 1
+    when /URD|DLU|RDL|LUR/ then length += 1
+    when /ULD|DRU|LDR|RUL/ then length -= 1
     end
-    nx, ny = move(x, y, dir, length)
-    trench << [nx, ny]
-    x = nx
-    y = ny
+    x, y = move(x, y, dir, length)
+    arr << [x, y]
   end
+end
 
+def find_area(trench)
   # Shoelace formula
   # # A = 0.5 * |(x1*y2 - x2*y1) + (x2*y3 - x3*y2) + ... + (xn*y1 - x1*yn)|
   trench.each_cons(2).reduce(0) { |sum, ((x1, y1), (x2, y2))| sum + (x1 * y2 - x2 * y1) }.abs / 2
 end
 
 directions = direction_groups(instructions)
-p find_area(instructions, directions)
+p find_area(dig_trench(instructions, directions))
 
 key = { '0' => 'R', '1' => 'D', '2' => 'L', '3' => 'U' }
 
 new_instructions = instructions.map { |l| l[2].match(/\(#(.*)\)/)[1] }
-  .map { |code| [key[code[-1]], code[...-1].to_i(16)] }
+                               .map { |code| [key[code[-1]], code[...-1].to_i(16)] }
 new_directions = direction_groups(new_instructions)
-p find_area(new_instructions, new_directions)
+p find_area(dig_trench(new_instructions, new_directions))
